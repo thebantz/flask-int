ec4a8c28695864ecc88cbd39e895509429ce257d
Nicole <nicoledbanta@gmail.com>
2020-07-01 21:46:14 -0700

    edit procfile v6

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
+from intro_to_flask import app
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
